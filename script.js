25a8179dedfeb0cee734b3917ab9ffa8477ab7bc
binshadhbu <binshadh_b210517cs@nitc.ac.in>
2022-12-18 18:05:35 +0530

    working finr everyhing

--- script.js
+++ script.js
@@ -9,41 +9,58 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+let map, mapEvent;
 
 navigator.geolocation.getCurrentPosition(
   function (position) {
-    console.log(position.coords.latitude,position.coords.longitude);
-   const coords=[11.2189,75.7268];
-    const map = L.map('map').setView([position.coords.latitude,position.coords.longitude],10);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    console.log(position.coords.latitude, position.coords.longitude);
+    const coords = [11.2189, 75.7268];
+     map = L.map("map").setView(
+      [position.coords.latitude, position.coords.longitude],
+      10
+    );
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-    
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    map.on("click", function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove("hidden");
+      inputDistance.focus();
+    });
 
-        map.on('click',function(mapEvent){
-            console.log(mapEvent);
-            const lat=mapEvent.latlng.lat;
-            const lng=mapEvent.latlng.lng;
-            L.marker([lat,lng]).addTo(map).bindPopup(L.popup({
-                maxWidth:250,
-                minWidth:100,
-                autoClose:false,
-                closeOnClick:false,
-                className:'running-popup'
-            })).setPopupContent('work').openPopup();
-        });
-        map.on('click',function(mapEvent){
-            form.classList.remove('hidden');
-            inputDistance.focus();
-        });
-
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
   },
   function () {
     alert("not working location");
   }
 );
 
-// ~
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    //inputCadence=inputDistance=inputDuration=inputElevation;
+
+  map.on("click", function (e) {
+    
+    console.log(mapEvent);
+    const lat = mapEvent.latlng.lat;
+    const lng = mapEvent.latlng.lng;
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("work")
+      .openPopup();
+  });
+});
+
